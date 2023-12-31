e4f23dd603f14bbd34592af6699b9b09b0fa7c40
Luke Wang <48535279+KingSkyrock@users.noreply.github.com>
2023-12-30 17:04:10 -0800

    Switched to greenlock-express and proxied express to next

--- server.js
+++ server.js
@@ -1,70 +1,45 @@
-const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
+const express = require('express');
  
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = process.env.PORT || 3000
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
-
-var appServ;
+const server = express();
 
 app.prepare().then(() => {
-  appServ = createServer(async (req, res) => {
+  server.all('*', (req, res) => {
     try {
       const parsedUrl = parse(req.url, true)
       const { pathname, query } = parsedUrl
  
       if (!pathname.includes("api")) {
-        await app.render(req, res, pathname, query)
+        app.render(req, res, pathname, query)
       } else {
-        await handle(req, res, parsedUrl)
+        handle(req, res, parsedUrl)
       }
     } catch (err) {
       console.error('Error occurred handling', req.url, err)
       res.statusCode = 500
       res.end('internal server error')
     }
-  })
-    .once('error', (err) => {
-      console.error(err)
-      process.exit(1)
-    })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
-    })
+  });
 })
 
 if (process.env.NODE_ENV === "production") {
-  const pkg = require("./package.json");
-  const Greenlock = require("@root/greenlock");
+  require("greenlock-express")
+      .init({
+          packageRoot: __dirname,
+          configDir: "./greenlock.d",
+          maintainerEmail: "koxaha7706@kembung.com",
 
-  var gl = Greenlock.create({
-    packageRoot: __dirname,
-    configDir: "./greenlock.d/",
-    packageAgent: pkg.name + "/" + pkg.version,
-    maintainerEmail: "koxaha7706@kembung.com",
-    cluster: false,
-    notify: function (event, details) {
-        if ("error" === event) {
-            console.error('greenlock notify :' + event, details);
-        }
-    },
-  });
-  
-  gl.manager.defaults({
-    subscriberEmail: 'koxaha7706@kembung.com',
-    agreeToTerms: true,
-    challenges: {
-      "http-01": {
-        module: "acme-http-01-standalone",
-      }
-    }
-  });
-
-  gl.add({
-    subject: 'sandboxsql.com',
-    altnames: ['sandboxsql.com', 'www.sandboxsql.com']
+          cluster: false
+      })
+      .serve(server);
+} else {
+  server.listen(port, function () {
+    console.log('App listening on port: ' + port);
   });
 }
