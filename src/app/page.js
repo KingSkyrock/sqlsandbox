fdefb2bedc5cd0dd959efa4e048bbc0dbb8948c0
Luke Wang <48535279+KingSkyrock@users.noreply.github.com>
2024-04-19 20:10:18 -0700

    Minor change to landing page

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,13 @@ export default class Home extends React.Component {
         <div className="home-container">
           <div>
             <div className="title">Welcome!</div>
-            <div className="subtitle">Create a new database or load an existing one</div>
+            <p className="explanation">
+              SandboxSQL is a online SQL editor and compiler where you can write and execute SQLite queries online.
+              <br/><br/>
+              You can use the template databases to learn, practice, or test queries. 
+              <br/><br/>
+              Start by creating a new database or loading an existing one.
+            </p>
           </div>
         </div>
       </div>
